1e41a6b28b40e7dc1fcea54588dff8e4c7e7cf7c
joxesantiago <jose.santiago90@upr.edu>
2020-10-29 17:12:58 -0400

    login with db 0.1v

--- dao/users.py
+++ dao/users.py
@@ -3,4 +3,3 @@ import psycopg2
 
 #Connect using psycopg2
 conn = psycopg2.connect("dbname=d1j12hfu81fusv  user=ybvcezhsbtthew password=c25e2e381493ce7f44698d77ca1f30ab3828f97820045c2102e9868d7aed22c8 host=ec2-18-210-90-1.compute-1.amazonaws.com")
-
--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, session, url_for
+import psycopg2
+from psycopg2.extensions import AsIs
+
+#Connect using psycopg2
+conn = psycopg2.connect("dbname=d1j12hfu81fusv  user=ybvcezhsbtthew password=c25e2e381493ce7f44698d77ca1f30ab3828f97820045c2102e9868d7aed22c8 host=ec2-18-210-90-1.compute-1.amazonaws.com")
+cursor = conn.cursor()
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    if request.method == 'POST':
+        if 'username' in request.form and 'password' in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            cursor.execute('SELECT * from accounts WHERE usernames=%s AND passwords=%s',(username,password))
+            info = cursor.fetchone()
+            if info['username'] == username and info['password'] == password:
+                return "login successful"
+            else:
+                return "login unsuccessful, please register"
+    return render_template("login.html")
+#
+# @app.route('/new')
+# def new_user():
+#     return render_template("register.html")
+#
+# @app.route('/profile')
+# def profile():
+#     return render_template("profile.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
