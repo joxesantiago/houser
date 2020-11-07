918f4690119b42dd5fcd7a76453b9a66e17f1cc3
joxesantiago <jose.santiago90@upr.edu>
2020-11-07 19:27:05 -0400

    last push

--- main.py
+++ main.py
@@ -5,31 +5,42 @@ conn = psycopg2.connect("dbname=d1j12hfu81fusv  user=ybvcezhsbtthew password=c25
 cursor = conn.cursor()
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        if 'username' in request.form and 'password' in request.form:
-            username = request.form['username']
-            password = request.form['password']
-            cursor.execute('SELECT * from accounts WHERE usernames=%s AND passwords=%s',(username,password))
-            credentials = cursor.fetchone()
-            print(credentials)
-            if credentials is not None:
-                if credentials[0] == username and credentials[1] == password:
-                    return "login successful"
-            else:
-                return "login unsuccessful, please register"
-    return render_template("login.html")
+    return render_template("index.html")
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         if 'username' in request.form and 'password' in request.form:
+#             username = request.form['username']
+#             password = request.form['password']
+#             cursor.execute('SELECT * from accounts WHERE usernames=%s AND passwords=%s',(username,password))
+#             credentials = cursor.fetchone()
+#             print(credentials)
+#             if credentials is not None:
+#                 if credentials[0] == username and credentials[1] == password:
+#                     session['username'] = True
+#                     return redirect(url_for('register'))
+#             else:
+#                 return redirect(url_for('register'))
+#     return render_template("login.html")
 #
 # @app.route('/new')
 # def new_user():
+#     if request.method == "POST":
+#         if "one" in request.method and "two" in request.form and "three" in request.form:
+#             username = request.form['one']
+#             password = request.form['three']
+#             cursor.execute("INSERT INTO d1j12hfu81fusv.accounts(usernames,passwords)VALUES(%s,%s,%s)",(username,password))
 #     return render_template("register.html")
 #
 # @app.route('/profile')
 # def profile():
-#     return render_template("profile.html")
-
+#     if session['username'] == True:
+#         return render_template("profile.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
